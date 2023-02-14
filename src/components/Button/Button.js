c4cf2402c20ac530d12e6e8a2eaaef616b105a28
natalia-pavlenko <npavl56@gmail.com>
2023-02-14 12:37:01 +0200

    Update Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,14 @@
 // import Loader from 'components/Loader';
 // import PropTypes from 'prop-types';
-const Button = ({ handleClickLoadMoreBtn }) => {
-  return (
-    <>
-      {showBtn && (
-        <button type="submit" onClick={this.handleClickLoadMoreBtn}>
-          Load more
-        </button>
-      )}
-    </>
-  );
-};
-export default Button;
+// const Button = ({ handleClickLoadMoreBtn }) => {
+//   return (
+//     <>
+//       {showBtn && (
+//         <button type="submit" onClick={this.handleClickLoadMoreBtn}>
+//           Load more
+//         </button>
+//       )}
+//     </>
+//   );
+// };
+// export default Button;
